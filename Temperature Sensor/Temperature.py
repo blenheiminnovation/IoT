665ba71bb73c43612b1a58136154d989b4112a04
Innovation Student <innovationstudent@outlook.com>
2022-08-03 16:49:32 +0100

    Added temperature sensor

--- Temperature Sensor/Temperature.py
+++ Temperature Sensor/Temperature.py
@@ -4,17 +4,23 @@ import time
 
 tempPin = ADC(Pin(26, Pin.IN))
 
-while(1):
-    ADCValue = tempPin.read_u16() #Reading the value in 16bits unsigned
+while 1:
+    ADCValue = tempPin.read_u16()  # Reading the value in 16bits unsigned
     print(tempPin.read_u16())
-    V0 = (ADCValue / 65535) * 3.3 #Calculating the reference voltage, using the ratio between the measured value and the maximum ADC value. The multiplying the input voltage by this ratio
+    V0 = (
+        ADCValue / 65535
+    ) * 3.3  # Calculating the reference voltage, using the ratio between the measured value and the maximum ADC value. The multiplying the input voltage by this ratio
     print(V0)
-    
-    resistance = ((3.3/V0) - 1) * (1 / 44900) #Using the input and reference voltage, as well as the series resistance
-    resistance = 1 / resistance 
+
+    resistance = ((3.3 / V0) - 1) * (
+        1 / 44900
+    )  # Using the input and reference voltage, as well as the series resistance
+    resistance = 1 / resistance
     print(resistance)
 
-    steinhart = resistance / 10000 #Using the calculated, series and nominal values in the Steinhart equation to find the temperature in Kelvin
+    steinhart = (
+        resistance / 10000
+    )  # Using the calculated, series and nominal values in the Steinhart equation to find the temperature in Kelvin
     print(steinhart)
     steinhart = math.log(steinhart)
     print(steinhart)
@@ -24,8 +30,7 @@ while(1):
     print(steinhart)
     steinhart = 1 / steinhart
     print(steinhart)
-    steinhart -= 273.15 #Converting from Kelvin into Centigrade
+    steinhart -= 273.15  # Converting from Kelvin into Centigrade
     print(steinhart)
     print(steinhart)
     time.sleep(10)
-    
--- Temperature Sensor/rakWirelessTemperature.py
+++ Temperature Sensor/rakWirelessTemperature.py
@@ -1,142 +1,158 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-#----------------------------------------------------------------------------
-#Created by David Green and Joe Lucas
-#Blenheim Innovation Team
-#Open Sensor Network
-#Created Date: 19/07/22
-#Version = '1.0'
-#----------------------------------------------------------------------------
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created by David Green and Joe Lucas
+# Blenheim Innovation Team
+# Open Sensor Network
+# Created Date: 19/07/22
+# Version = '1.0'
+# ----------------------------------------------------------------------------
 """rakWirelessTemperature.py: This program uses a Temperature Sensor (TS) (NTC-MF52 3950
 ), Raspbery Pi Pico, and RAK4270H to read and transmit data, from the TS, to The Things Network and Datacake"""
-#----------------------------------------------------------------------------
-#Imports
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# Imports
+# ----------------------------------------------------------------------------
 from machine import UART, ADC, Pin
 import time
 import utime
 import struct
 from binascii import hexlify
+
 ####
 
-DevEUI = '60C5A8FFFE78F38E' #Insert your DEVEUI here
-AppEUI = '1000000000000009' #Insert your APPEUI here
-AppKey = '170E5E7876C0A42BD9CEB01CE2786544' #Insert your APPKEY here
+DevEUI = "60C5A8FFFE78F38E"  # Insert your DEVEUI here
+AppEUI = "1000000000000009"  # Insert your APPEUI here
+AppKey = "170E5E7876C0A42BD9CEB01CE2786544"  # Insert your APPKEY here
 decoded_data = ""
-led = Pin(25, Pin.OUT) #Connects to the onboard LED to give a physical signal of when data is being transmitted
+led = Pin(
+    25, Pin.OUT
+)  # Connects to the onboard LED to give a physical signal of when data is being transmitted
 tempPin = ADC(Pin(26, Pin.IN))
 led.value(0)
 
-def hexConvert(string): #Acts to convert the payload into hexadecimal format, so that it can be sent to The Things Network and Datacake to be decoded on each of these platforms
+
+def hexConvert(
+    string,
+):  # Acts to convert the payload into hexadecimal format, so that it can be sent to The Things Network and Datacake to be decoded on each of these platforms
     encoded = string.encode()
     hexValue = hexlify(string).decode()
     print(hexValue)
     return hexValue
 
-def findTemp(tempPin):
-    ADCValue = tempPin.read_u16() #Reading the value in 16bits unsigned
-    V0 = (ADCValue / 65535) * 3.3 #Calculating the reference voltage, using the ratio between the measured value and the maximum ADC value. The multiplying the input voltage by this ratio
-    
-    resistance = ((3.3/V0) - 1) * (1 / 44900) #Using the input and reference voltage, as well as the series resistance
-    resistance = 1 / resistance 
 
-    steinhart = resistance / 10000 #Using the calculated, series and nominal values in the Steinhart equation to find the temperature in Kelvin
+def findTemp(tempPin):
+    ADCValue = tempPin.read_u16()  # Reading the value in 16bits unsigned
+    V0 = (
+        ADCValue / 65535
+    ) * 3.3  # Calculating the reference voltage, using the ratio between the measured value and the maximum ADC value. The multiplying the input voltage by this ratio
+
+    resistance = ((3.3 / V0) - 1) * (
+        1 / 44900
+    )  # Using the input and reference voltage, as well as the series resistance
+    resistance = 1 / resistance
+
+    steinhart = (
+        resistance / 10000
+    )  # Using the calculated, series and nominal values in the Steinhart equation to find the temperature in Kelvin
     steinhart = math.log(steinhart)
     steinhart /= 3950
     steinhart += 1 / (25 + 273.15)
     steinhart = 1 / steinhart
-    steinhart -= 273.15 #Converting from Kelvin into Centigrade
+    steinhart -= 273.15  # Converting from Kelvin into Centigrade
     return steinhart
+
 
 uart = UART(0, 115200)  # use RPI PICO GP0 and GP1
 
 # Use lines below to display the firmware version and config settings
-#uart.write('at+version\r\n')
-#uart.write('at+get_config=lora:status\r\n')
-#data = uart.read()
-#if data:
+# uart.write('at+version\r\n')
+# uart.write('at+get_config=lora:status\r\n')
+# data = uart.read()
+# if data:
 #    decoded_data = data.decode('utf-8')
 #    print("Check Config:" + decoded_data)
 
 ## Setup the wireless module OTAA, Class, Region, keys##
-uart.write('at+set_config=lora:join_mode:0\r\n')
+uart.write("at+set_config=lora:join_mode:0\r\n")
 print("set to OTAA")
-while(decoded_data != "OK\r\n"):
-    
+while decoded_data != "OK\r\n":
+
     data = uart.read()
-    
+
     if data:
-        decoded_data = data.decode('utf-8')
+        decoded_data = data.decode("utf-8")
         print("OTAA done!\r\n" + decoded_data)
-        
+
 decoded_data = ""
-uart.write('at+set_config=lora:class:0\r\n')
+uart.write("at+set_config=lora:class:0\r\n")
 print("set to Class A")
 
 
-while(decoded_data != "OK\r\n"):
+while decoded_data != "OK\r\n":
     data = uart.read()
     if data:
-        decoded_data = data.decode('utf-8')
+        decoded_data = data.decode("utf-8")
         print("Class A done!\r\n" + decoded_data)
 
 decoded_data = ""
-uart.write('at+set_config=lora:region:EU868\r\n')
+uart.write("at+set_config=lora:region:EU868\r\n")
 print("set to EU868 region")
-while(decoded_data != "OK\r\n"):
+while decoded_data != "OK\r\n":
     data = uart.read()
     if data:
-        decoded_data = data.decode('utf-8')
+        decoded_data = data.decode("utf-8")
         print("EU868 done!\r\n" + decoded_data)
 
 decoded_data = ""
-uart.write('at+set_config=lora:dev_eui:' + DevEUI +'\r\n')
+uart.write("at+set_config=lora:dev_eui:" + DevEUI + "\r\n")
 print("set to DEVEUI")
-while(decoded_data != "OK\r\n"):
+while decoded_data != "OK\r\n":
     data = uart.read()
     if data:
-        decoded_data = data.decode('utf-8')
+        decoded_data = data.decode("utf-8")
         print("DEVUI done!\r\n" + decoded_data)
 
 decoded_data = ""
-uart.write('at+set_config=lora:app_eui:' + AppEUI +'\r\n')
+uart.write("at+set_config=lora:app_eui:" + AppEUI + "\r\n")
 print("set to APPEUI")
-while(decoded_data != "OK\r\n"):
+while decoded_data != "OK\r\n":
     data = uart.read()
     if data:
-        decoded_data = data.decode('utf-8')
+        decoded_data = data.decode("utf-8")
         print("APPEUI done!\r\n" + decoded_data)
 
 decoded_data = ""
-uart.write('at+set_config=lora:app_key:' + AppKey +'\r\n')
+uart.write("at+set_config=lora:app_key:" + AppKey + "\r\n")
 print("set to APPKEY")
-while(decoded_data != "OK\r\n"):
+while decoded_data != "OK\r\n":
     data = uart.read()
     if data:
-        decoded_data = data.decode('utf-8')
+        decoded_data = data.decode("utf-8")
         print("APPKEY done!\r\n" + decoded_data)
 ## END OF SETTING UP ##
 
 ## TRY TO JOIN THE NETWORK!##
 def joinNetwork():
     decoded_data = ""
-    uart.write('at+join\r\n')
+    uart.write("at+join\r\n")
     print("joining...")
-    while(decoded_data != "OK Join Success\r\n"):
+    while decoded_data != "OK Join Success\r\n":
         try:
             data = uart.read()
             if data:
-                decoded_data = data.decode('utf-8')
-                if decoded_data == 'OK Join Success\r\n':
+                decoded_data = data.decode("utf-8")
+                if decoded_data == "OK Join Success\r\n":
                     print(decoded_data)
                     time.sleep(30)
-                if decoded_data == 'ERROR: 99\r\n':
-                    raise Exception('Join Error')
-                
-                while(1):
+                if decoded_data == "ERROR: 99\r\n":
+                    raise Exception("Join Error")
+
+                while 1:
                     led.value(1)
                     temperature = round(findTemp(tempPin), 2)
-                    temperatureString = ("TMP~~TST~~" + temperature + "~~0~~0") #This acts as the payload for trasnmission and is in the structure: Sensor Type, Sensor Name, Value 1, Value 2, Value 3
+                    temperatureString = (
+                        "TMP~~TST~~" + temperature + "~~0~~0"
+                    )  # This acts as the payload for trasnmission and is in the structure: Sensor Type, Sensor Name, Value 1, Value 2, Value 3
                     payload = hexConvert(temperatureString)
                     uart.write(
                         "at+send=lora:2:" + payload + "\r\n"
