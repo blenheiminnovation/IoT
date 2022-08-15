dd1a41c2221323534d47802ae0755e63d1198844
Innovation Student <innovationstudent@outlook.com>
2022-08-15 15:12:56 +0100

    Add Comments

--- Arduino Gas Sensor/NO2.ino
+++ Arduino Gas Sensor/NO2.ino
@@ -2,19 +2,18 @@
 #include <Wire.h>
 #include "MutichannelGasSensor.h"
 
-// Set your AppEUI and AppKey
-const char *appEui = "0000000000000000";
-const char *appKey = "C28C6C03A5D84A53010C44F9308F12F6";
+const char *appEui = ""; // Insert your APPEUI here
+const char *appKey = ""; // Insert your APPKEY here
 
 #define loraSerial Serial1
 #define debugSerial Serial
 
 // Replace REPLACE_ME with TTN_FP_EU868 or TTN_FP_US915
-#define freqPlan TTN_FP_EU868
+#define freqPlan REPLACE_ME
 
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
 
-void setup()
+void setup() // Send a join request to TTN and set up the gas sensor
 {
   loraSerial.begin(57600);
   debugSerial.begin(9600);
@@ -28,9 +27,9 @@ void setup()
 
   debugSerial.println("-- JOIN");
   ttn.join(appEui, appKey);
-  Serial.begin(115200);  // start serial for output
+  Serial.begin(115200); // start serial for output
   Serial.println("power on!");
-  gas.begin(0x04);//the default I2C address of the slave is 0x04
+  gas.begin(0x04); // the default I2C address of the slave is 0x04
   gas.powerOn();
   Serial.print("Firmware Version = ");
   Serial.println(gas.getVersion());
@@ -39,33 +38,36 @@ void setup()
 void loop()
 {
   float c;
-  
+
   c = gas.measure_NO2();
   Serial.print("The concentration of NO2 is ");
-  if (c >= 0) {
-      Serial.print(c);
-  } else {
-      Serial.print("invalid");
+  if (c >= 0)
+  { // Check if the value is valid
+    Serial.print(c);
+  }
+  else
+  {
+    Serial.print("invalid");
   }
   Serial.println(" ppm");
-  
+
   debugSerial.println("-- LOOP");
 
   // Prepare payload of 1 byte to indicate LED status
-  String valueString = String(c, 2);
-  char value[4] = {valueString[0], valueString[1], valueString[2], valueString[3]};
+  String valueString = String(c, 2);                                                // Convert the 2 decimal place float into a string
+  char value[4] = {valueString[0], valueString[1], valueString[2], valueString[3]}; // Split the value string up into an array of the characters in the string
   char sensor[3] = {'N', 'O', '2'};
-  byte payload[7];
+  byte payload[7]; // Define the payload and how many bytes it is
   payload[0] = sensor[0];
   payload[1] = sensor[1];
   payload[2] = sensor[2];
   payload[3] = value[0];
   payload[4] = value[1];
   payload[5] = value[2];
-  payload[6] = value[3];
+  payload[6] = value[3]; // Add all of the characters to the payload
 
-  // Send it off
+  // Transmit the payload to TTN
   ttn.sendBytes(payload, sizeof(payload));
 
-  delay(900000);
+  delay(900000); // Force the device to wait 15 minutes before reading and trasnmitting data again
 }
